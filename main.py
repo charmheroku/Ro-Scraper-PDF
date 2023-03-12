3b47372fa4139974a03267c147fa8f9b61c8e1af
Oleksii Ivanov <pcharm563@gmail.com>
2023-03-12 11:52:05 +0100

    get data with bs4

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import logging
 import requests
 
+from bs4 import BeautifulSoup
+
 
 # Logging
 logger = logging.getLogger(__name__)
@@ -19,9 +21,22 @@ def get_html(url):
         logger.exception(f"Problem was with document {url}")
 
 
+def get_data(html):
+    urls = []
+    soup = BeautifulSoup(html, "html.parser")
+    lists = soup.find("div", {"class": "penci-entry-content entry-content"})
+    for li in lists.find_all("li"):
+        for a in li.find_all("a", href=True):
+            if "href" in a.attrs:
+                url = a.get("href")
+                number = a.text
+                urls.append([url, number])
+    return urls
+
+
 def main():
     url = os.getenv("URL")
-    html = get_html(url)
+    urls = get_data(get_html(url))
 
 
 if __name__ == "__main__":
